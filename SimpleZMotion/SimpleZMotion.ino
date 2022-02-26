0b262e190dd5857510b17a59d0c1184e4a9dc8d0
philipdoucla <93559391+philipdoucla@users.noreply.github.com>
2022-02-25 23:00:32 -0800

    Simple Z Motion Code
    Moves the arms up and down

---
+++ SimpleZMotion/SimpleZMotion.ino
@@ -0,0 +1,49 @@
+const int stepPinX = 11;
+const int dirPinX = 12;
+const int stepPinY = 9;
+const int dirPinY = 10;
+const int stepPinZ = 8;
+const int dirPinZ = 7;
+
+void setup() {
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(dirPinX, LOW);
+  digitalWrite(dirPinY, LOW);
+  digitalWrite(dirPinZ, LOW);
+  
+  for(int i = 0; i < 200; i++) {
+    digitalWrite(stepPinX, HIGH);
+    digitalWrite(stepPinY, HIGH);
+    digitalWrite(stepPinZ, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPinX, LOW);
+    digitalWrite(stepPinY, LOW);
+    digitalWrite(stepPinZ, LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+
+  digitalWrite(dirPinX, HIGH);
+  digitalWrite(dirPinY, HIGH);
+  digitalWrite(dirPinZ, HIGH);
+
+  for(int i = 0; i < 200; i++) {
+    digitalWrite(stepPinX, HIGH);
+    digitalWrite(stepPinY, HIGH);
+    digitalWrite(stepPinZ, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPinX, LOW);
+    digitalWrite(stepPinY, LOW);
+    digitalWrite(stepPinZ, LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+}
